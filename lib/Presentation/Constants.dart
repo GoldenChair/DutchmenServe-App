0353dd0c97966c26a205830c36fa8a28172be8fb
Kendall <kll008@lvc.edu>
2021-10-22 11:11:38 -0400

    constants

--- lib/Presentation/Constants.dart
+++ lib/Presentation/Constants.dart
@@ -1,4 +1,5 @@
 import 'package:dutchmenserve/models/interest.dart';
+import 'package:flutter/material.dart';
 
 /*
 This class defines constants.
@@ -21,6 +22,45 @@ class Constants {
     Interest('Service Trips', 59153, '#00897b', '#8dd3c7'),
     Interest('Veterans', 61283, '#8e24aa', '#bc80bd'),
     Interest('Other', 59526, '#546e7a', '#d9d9d9'),
+  ];
+
+  final List<IconData> icons = [
+    Icons.pets,
+    Icons.accessible,
+    Icons.school,
+    Icons.local_restaurant,
+    Icons.healing,
+    Icons.home,
+    Icons.face,
+    Icons.explore,
+    Icons.stars,
+    Icons.more_horiz,
+  ];
+
+  final List<Color> colors = [
+    Colors.pink[600],
+    Colors.blueAccent[200],
+    Colors.orangeAccent[700],
+    Colors.lime,
+    Colors.redAccent,
+    Colors.greenAccent[700],
+    Colors.deepPurple[400],
+    Colors.teal[600],
+    Colors.purple[600],
+    Colors.blueGrey[600],
+  ];
+
+  final List<Color> fillColors = [
+    Colors.pink[100], //Color(0xfffccde5),
+    Colors.blue[100], //Color(0xff80b1d3),
+    Colors.orange[100], //Color(0xffffffb3),
+    Colors.lime[100], //Color(0xfffdb462),
+    Colors.red[100], //Color(0xfffb8072),
+    Colors.green[100], //Color(0xffb3de69),
+    Colors.deepPurple[100], //Color(0xffbebada),
+    Colors.teal[100], //Color(0xff8dd3c7),
+    Colors.purple[100], //Color(0xffbc80bd),
+    Colors.blueGrey[100], //Color(0xffd9d9d9),
   ];
 
   // index colors by interest id
