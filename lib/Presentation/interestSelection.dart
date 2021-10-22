fa457a165458252e19a2f0bfbbaa473fe4325a85
Kendall <kll008@lvc.edu>
2021-10-22 11:24:58 -0400

    constants in interestpages

--- lib/Presentation/interestSelection.dart
+++ lib/Presentation/interestSelection.dart
@@ -6,6 +6,8 @@ import 'package:dutchmenserve/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'Constants.dart';
+
 /*
 Page to select interests by having the user click on the interests.
  */
@@ -24,57 +26,10 @@ class _SelectInterestsState extends State<SelectInterests> {
   User user;
   _SelectInterestsState(this.user);
 
-  final List<Interest> interests = [
-    Interest('Animals', 59662, '#d81b60', '#fccde5'),
-    Interest('Disabilities', 58718,'#448aff', '#80b1d3'),
-    Interest('Education', 59816, '#ff6d00', '#ffffb3'),
-    Interest('Food', 59429, '#cddc39', '#fdb462'),
-    Interest('Health\nWellness', 59308, '#ff5252', '#fb8072'),
-    Interest('Housing', 59322, '#00c853', '#b3de69'),
-    Interest('Older Adults', 59162, '#7e57c2', '#bebada'),
-    Interest('Service Trips', 59153, '#00897b', '#8dd3c7'),
-    Interest('Veterans', 61283, '#8e24aa', '#bc80bd'),
-    Interest('Other', 59526, '#546e7a', '#d9d9d9'),
-  ];
-
-  final List<IconData> icons = [
-    Icons.pets,
-    Icons.accessible,
-    Icons.school,
-    Icons.local_restaurant,
-    Icons.healing,
-    Icons.home,
-    Icons.face,
-    Icons.explore,
-    Icons.stars,
-    Icons.more_horiz,
-  ];
-
-  final List<Color> colors = [
-    Colors.pink[600],
-    Colors.blueAccent[200],
-    Colors.orangeAccent[700],
-    Colors.lime,
-    Colors.redAccent,
-    Colors.greenAccent[700],
-    Colors.deepPurple[400],
-    Colors.teal[600],
-    Colors.purple[600],
-    Colors.blueGrey[600],
-  ];
-
-  final List<Color> fillColors = [
-    Colors.pink[100], //Color(0xfffccde5),
-    Colors.blue[100], //Color(0xff80b1d3),
-    Colors.orange[100], //Color(0xffffffb3),
-    Colors.lime[100], //Color(0xfffdb462),
-    Colors.red[100], //Color(0xfffb8072),
-    Colors.green[100], //Color(0xffb3de69),
-    Colors.deepPurple[100], //Color(0xffbebada),
-    Colors.teal[100], //Color(0xff8dd3c7),
-    Colors.purple[100], //Color(0xffbc80bd),
-    Colors.blueGrey[100], //Color(0xffd9d9d9),
-  ];
+  final colors = Constants().colors;
+  final interests = Constants().interests;
+  final icons = Constants().icons;
+  final fillColors = Constants().fillColors;
 
   List<bool> _s = List.generate(12, (index) => false);
 
