ff746c24b77105de909d198c43041c641d753e6a
Darrell Laptop <dw005@lvc.edu>
2021-10-28 22:00:54 -0400

    updates user in repo using cubit

--- lib/Presentation/organizationSelect.dart
+++ lib/Presentation/organizationSelect.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'package:dutchmenserve/Infrastructure/cubit/users_cubit.dart';
 import 'package:dutchmenserve/Presentation/widgets.dart';
 import 'package:dutchmenserve/Presentation/homePage.dart';
 import 'package:dutchmenserve/models/user.dart';
@@ -16,18 +17,15 @@ Offers option to set up later.
 */
 
 class SetUpOrgPage extends StatefulWidget {
-  final User user;
-  SetUpOrgPage({Key key, @required this.user}) : super(key: key);
-
+  SetUpOrgPage({Key key}) : super(key: key);
   @override
   SetUpOrgState createState() {
-    return SetUpOrgState(user);
+    return SetUpOrgState();
   }
 }
 
 class SetUpOrgState extends State<SetUpOrgPage> {
-  User user;
-  SetUpOrgState(this.user);
+  SetUpOrgState();
 
   // final List<String> entries = <String>[
   //   'Alpha Phi Omega',
@@ -56,12 +54,13 @@ class SetUpOrgState extends State<SetUpOrgPage> {
   // 13 is a hardcoded number
   List<bool> _isChecked = List.generate(13, (index) => false);
 
-  void sendToCubit(User user) {
+  void sendToCubit() {
     // tell user cubit to add user
   }
 
   void next() {
-    sendToCubit(user);
+    sendToCubit();
+    User user = null;
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Color(0xff002A4E),
       systemNavigationBarColor: Color(0xfff9f9f9),
@@ -85,7 +84,7 @@ class SetUpOrgState extends State<SetUpOrgPage> {
             new Expanded(
               child: BlocBuilder<OrganizationCubit, OrganizationState>(
                   builder: (context, state) {
-                if (state is LoadedState) {
+                if (state is OrgLoadedState) {
                   List<String> entries = getEntries(state.orgs);
                   return ListView.separated(
                     padding: const EdgeInsets.all(20.0),
@@ -121,18 +120,28 @@ class SetUpOrgState extends State<SetUpOrgPage> {
             Container(
               margin: EdgeInsets.only(top: 10),
               child: BlocBuilder<OrganizationCubit, OrganizationState>(
-                  builder: (context, state) {
-                if (state is LoadedState) {
-                  List<String> entries = getEntries(state.orgs);
-                  return NormalButton(
-                    'Next',
-                    () {
-                      for (int i = 0; i < entries.length; i++) {
-                        if (_isChecked[i]) user.organizations.add(i);
-                      }
-                      next();
-                    },
-                  );
+                  builder: (context, stateOrg) {
+                if (stateOrg is OrgLoadedState) {
+                  List<String> entries = getEntries(stateOrg.orgs);
+                  return BlocBuilder<UsersCubit, UsersState>(
+                      builder: (context, state) {
+                    if (state is UsersLoadedState) {
+                      return NormalButton(
+                        'Next',
+                        () {
+                          List<int> orgs = [];
+                          for (int i = 0; i < entries.length; i++) {
+                            if (_isChecked[i]) orgs.add(i);
+                          }
+                          state.curUser.organizations = orgs;
+                          editUser(context, state.curUser);
+                          next();
+                        },
+                      );
+                    } else {
+                      return Text("error");
+                    }
+                  });
                 } else {
                   return Text("test");
                 }
@@ -153,5 +162,10 @@ class SetUpOrgState extends State<SetUpOrgPage> {
             ),
           ],
         ));
+  }
+
+  void editUser(BuildContext context, User user) {
+    final usersCubit = context.read<UsersCubit>();
+    usersCubit.editUser(user);
   }
 }
